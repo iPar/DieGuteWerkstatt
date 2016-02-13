89ff52334d2b8266112733c193ff57028b413500
ipar <parha100@mail.chapman.edu>
2016-02-13 12:01:27 -0800

    Added initial three (3) challenge solutions to javascripting

---
+++ Javascripting/02-Variables.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/***********************************************************
+ *
+ * In which the use of variables is exemplified.
+ *
+ **********************************************************/
+
+var example = 'some string';                                //  Variable 'example' is defined with a string as its value
+
+console.log(example);                                       //  Contents of 'example' are logged to the console (stdout)
+
---
+++ Javascripting/03-Strings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/***********************************************************
+ *
+ * In which the use of strings is exemplified.
+ *
+ **********************************************************/
+
+var someString = 'this is a string';                        //  Variable 'someString' is defined with a string as its value
+
+console.log(someString);                                    //  Contents of 'someString' are logged to the console (stdout)
+
+
